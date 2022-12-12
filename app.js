b387db1437e7f25217c9c0eb099c4260e4252c6b
Ursula Muts <ursula.muts@voco.ee>
2022-12-12 10:39:39 +0200

    delete dom

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 const form = document.querySelector('form')
 form.addEventListener('submit', addTask)
 
-
 function addTask(event) {
     const taskText = document.querySelector('#task').value
-    console.log(taskText)
+
     let li = document.createElement('li')
     li.className = 'collection-item'
-    let liText =document.createTextNode(taskText)
+    let liText = document.createTextNode(taskText)
     li.appendChild(liText)
 
-    let a = document.createElement("a")
-    a.className = 'teal-text darken-2 secondary-content'
+    let a = document.createElement('a')
+    a.className = 'teal-text lighten-2 secondary-content'
     let aText = document.createTextNode('X')
     a.appendChild(aText)
     a.setAttribute('href', '#')
+
     li.appendChild(a)
+
+    const ul = document.querySelector('ul')
     ul.appendChild(li)
 
     document.querySelector('#task').value = ''
-
     event.preventDefault()
-
-
-
 }
