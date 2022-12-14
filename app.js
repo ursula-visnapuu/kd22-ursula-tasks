6af99111a660c8857299c30b119a924e6394833d
Ursula Muts <ursula.muts@voco.ee>
2022-12-14 10:18:01 +0200

    Create reload page get all tasks from localStorage

--- app.js
+++ app.js
@@ -7,6 +7,35 @@ ul.addEventListener('click', deleteTask)
 const deleteTasks = document.querySelector('#clear-all-tasks')
 deleteTasks.addEventListener('click', deleteAllTasks)
 
+document.addEventListener('DOMContentLoaded', getTasks)
+
+function getTasks(event){
+    // add task to localStorage
+    let tasks // array for user inputs
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    // loop array for each element value
+    tasks.forEach(function (task){
+        let li = document.createElement('li')
+        li.className = 'collection-item'
+        let liText = document.createTextNode(task)
+        li.appendChild(liText)
+
+        let a = document.createElement('a')
+        a.className = 'teal-text lighten-2 secondary-content'
+        let aText = document.createTextNode('X')
+        a.appendChild(aText)
+        a.setAttribute('href', '#')
+
+        li.appendChild(a)
+
+        ul.appendChild(li)
+    })
+}
+
 function deleteAllTasks(event){
     // while(ul.firstElementChild){
     //     ul.removeChild(ul.firstElementChild)
@@ -25,8 +54,10 @@ function deleteTask(event){
 
 
 function addTask(event) {
+    // user input
     const taskText = document.querySelector('#task').value
 
+    // add DOM element - begin
     let li = document.createElement('li')
     li.className = 'collection-item'
     let liText = document.createTextNode(taskText)
@@ -41,6 +72,17 @@ function addTask(event) {
     li.appendChild(a)
 
     ul.appendChild(li)
+    // add DOM element - end
+
+    // add task to localStorage
+    let tasks // array for user inputs
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.push(taskText)
+    localStorage.setItem('tasks', JSON.stringify(tasks))
 
     document.querySelector('#task').value = ''
     event.preventDefault()
